6b413505f29f8f06d75fceadb83b52c8692e7266
Truong Tran <tct216@email.vccs.edu>
2015-04-25 21:20:47 -0400

    THis is the driver for assignment 5

---
+++ Assignment5Driver.java
@@ -0,0 +1,21 @@
+import javax.swing.SwingUtilities;
+
+
+public class Assignment5Driver
+{
+
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Assignment5();					
+			}
+		
+		});
+
+	}
+
+}
